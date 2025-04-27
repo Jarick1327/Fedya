7c69df98dab51e60a6265ccec474f290da279c13
Jarick1327 <159236528+Jarick1327@users.noreply.github.com>
2025-04-27 17:55:43 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //потоки воода и вывода
+using namespace std; //стандартный вывод cin и cout
+#include <stdio.h> //printf и scanf
+
+int main(int argc, char *argv[])
+{
+ cout << (" Hello, world!");
+ 
+ printf("\n Hello, Comp!");
+
+
+return 0;
+}
